2fdd2c0068513a8853cc6d089ba6181478e8f928
mishraaditya595 <mishraaditya595@gmail.com>
2020-09-07 19:17:44 +0530

    Added toolbar

--- app/src/main/java/com/vob/scanner/ui/MainActivity.java
+++ app/src/main/java/com/vob/scanner/ui/MainActivity.java
@@ -1,49 +1,36 @@
 package com.vob.scanner.ui;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Base64;
-import android.widget.ImageView;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.tabs.TabLayout;
-import com.scanlibrary.ScanActivity;
-import com.scanlibrary.ScanConstants;
 import com.vob.scanner.R;
 import com.vob.scanner.adapter.TabAdapter;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
-import static com.vob.scanner.constants.Constants.OPEN_THING;
-
 public class MainActivity extends AppCompatActivity {
 
-    ImageView scannedImageView;
     TabLayout tabLayout;
     ViewPager viewPager;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tabLayout = findViewById(R.id.main_tablayout);
-        viewPager = findViewById(R.id.viewPager);
+        initialiseFields();
+        setupToolbar();
+        setupTabLayout();
+    }
+
+    private void setupTabLayout() {
         tabLayout.addTab(tabLayout.newTab().setText("Scan Documents"));
         tabLayout.addTab(tabLayout.newTab().setText("Copy Text"));
         tabLayout.addTab(tabLayout.newTab().setText("Scan QR"));
-
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
         TabAdapter adapter = new TabAdapter(getSupportFragmentManager(),this,tabLayout.getTabCount());
         viewPager.setAdapter(adapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
@@ -52,20 +39,23 @@ public class MainActivity extends AppCompatActivity {
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
             }
-
             @Override
             public void onTabUnselected(TabLayout.Tab tab) { }
-
             @Override
             public void onTabReselected(TabLayout.Tab tab) { }
         });
+    }
 
-        /*scannedImageView = findViewById(R.id.scannedImageView);
-        findViewById(R.id.open_image).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFiles();
-            }
-        });*/
+    private void setupToolbar() {
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setTitleTextAppearance(getApplicationContext(), R.style.TextAppearance_AppCompat_Title);
+        toolbar.setTitleTextColor(0xFFFFFFFF);
+        setSupportActionBar(toolbar);
+    }
+
+    private void initialiseFields() {
+        tabLayout = findViewById(R.id.main_tablayout);
+        viewPager = findViewById(R.id.viewPager);
+        toolbar = findViewById(R.id.main_toolbar);
     }
 }
