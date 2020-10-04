55545bcc5add8bcaf9adeca45322cacadbbb5950
Aditya Mishra <mishraaditya595@gmail.com>
2020-10-04 20:30:13 +0530

    more changes

---
+++ monscanner/build/generated/source/buildConfig/release/com/monscanner/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.monscanner;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.monscanner";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
