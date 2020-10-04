3378be2d299455aaf2bff9c741cd4bc3506f5e56
Aditya Mishra <mishraaditya595@gmail.com>
2020-10-04 20:27:12 +0530

    changes

--- app/src/main/java/com/vob/scanit/ui/fragments/OCRFragment.kt
+++ app/src/main/java/com/vob/scanit/ui/fragments/OCRFragment.kt
@@ -116,7 +116,7 @@ class OCRFragment : Fragment() {
         {
             //prepareTessData()
             //startOCR(outputFileDir)
-            val imageUri: Uri = Objects.requireNonNull(data!!.extras).getParcelable(ScanActivity.SCAN_RESULT)!!
+            val imageUri: Uri = Objects.requireNonNull(data!!.extras)?.getParcelable(ScanActivity.SCAN_RESULT)!!
             val imageStream: InputStream = activity!!.contentResolver.openInputStream(imageUri)!!
             val scannedImage = BitmapFactory.decodeStream(imageStream)
             activity!!.contentResolver.delete(imageUri, null, null)
