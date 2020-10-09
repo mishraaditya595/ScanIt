5d97972013d632744b8956f9366f7d9a193cf4ab
Aditya Mishra <mishraaditya595@gmail.com>
2020-10-09 18:08:07 +0530

    implemented QRFragment.kt

---
+++ app/src/main/java/com/vob/scanit/ui/activities/ShowQRData.kt
@@ -0,0 +1,24 @@
+package com.vob.scanit.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.vob.scanit.R
+
+class ShowQRData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_q_r_data)
+
+        val data = intent.getStringExtra("data")
+        val tv = findViewById<TextView>(R.id.tv)
+        if (data != null)
+        {
+
+            tv.text = data
+        }
+        else
+            tv.text = "Nothing found"
+
+    }
+}
--- app/src/main/java/com/vob/scanit/ui/fragments/QRFragment.kt
+++ app/src/main/java/com/vob/scanit/ui/fragments/QRFragment.kt
@@ -1,58 +1,131 @@
 package com.vob.scanit.ui.fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+//import com.blikoon.qrcodescanner.QrCodeActivity
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
 import com.vob.scanit.R
+//import com.google.zxing.integration.android.IntentIntegrator
+//import com.vob.scanit.R
+import com.vob.scanit.ui.activities.ShowQRData
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [QRFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class QRFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    //private val REQUEST_CODE_QR_SCAN = 101
+    //lateinit var scannerView: ScannerView
+    private lateinit var codeScanner: CodeScanner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_q_r, container, false)
+
+        //val i = Intent(activity, QrCodeActivity::class.java)
+        //startActivityForResult(i, REQUEST_CODE_QR_SCAN)
+
+        //val button = view.findViewById<Button>(R.id.startscan_btn)
+        //button.setOnClickListener { letsScan() }
+
+        //scannerView = view.findViewById(R.id.scannerView)
+        //startScanner()
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val scannerView = view.findViewById<CodeScannerView>(R.id.scanView)
+        val activity = requireActivity()
+        codeScanner = CodeScanner(activity, scannerView)
+        codeScanner.decodeCallback = DecodeCallback {
+            activity.runOnUiThread {
+                Toast.makeText(activity, it.text, Toast.LENGTH_LONG).show()
+            }
+        }
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_q_r, container, false)
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment QRFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                QRFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
     }
+
+    /*private fun startScanner() {
+        scannerView.apply {
+            //addObserver(context)
+            onResultListener {
+                Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun restartScanner() {
+        scannerView.resumeScannerView()
+    }*/
+
+
+    /*private fun letsScan() {
+        val integrator = IntentIntegrator(activity)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
+        integrator.setPrompt("scan")
+        integrator.setCameraId(0)
+        integrator.setBeepEnabled(false)
+        integrator.setBarcodeImageEnabled(false)
+        integrator.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        val data = scanResult.contents
+        if (data!=null)
+            view!!.findViewById<TextView>(R.id.result_tv).text = data
+        else
+            view!!.findViewById<TextView>(R.id.result_tv).text = "Error"
+
+    }*/
+
+    /*override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode != Activity.RESULT_OK) {
+            //Log.d(LOGTAG, "COULD NOT GET A GOOD RESULT.")
+            if (data == null)
+                return
+            val result = data.getStringExtra("com.blikoon.qrcodescanner.error_decoding_image")
+            if (result != null)
+            {
+                Toast.makeText(context,"QR code could not be scanned.", Toast.LENGTH_SHORT).show()
+            }
+            return
+        }
+        if (requestCode == REQUEST_CODE_QR_SCAN)
+        {
+            if (data == null) return
+            //Getting the passed result
+            val result = data.getStringExtra("com.blikoon.qrcodescanner.got_qr_scan_relult")
+            //Log.d(LOGTAG, "Have scan result in your app activity :$result")
+            val intent = Intent(QRFragment().context, ShowQRData::class.java)
+            intent.putExtra("data",result)
+            startActivity(intent)
+            //Toast.makeText(QRFragment().context,result, Toast.LENGTH_SHORT).show()
+        }
+    }*/
 }
