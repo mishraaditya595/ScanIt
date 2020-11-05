4d55bc8b7b03bf0357e5d575c6b49cc6534189e7
Aditya Mishra <mishraaditya595@gmail.com>
2020-11-05 10:03:14 +0530

    Updates to Gradle

--- monscanner/build/generated/source/buildConfig/debug/com/monscanner/BuildConfig.java
+++ monscanner/build/generated/source/buildConfig/debug/com/monscanner/BuildConfig.java
@@ -7,6 +7,4 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String LIBRARY_PACKAGE_NAME = "com.monscanner";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.0";
 }
--- opencv/build/generated/source/buildConfig/debug/org/opencv/BuildConfig.java
+++ opencv/build/generated/source/buildConfig/debug/org/opencv/BuildConfig.java
@@ -7,6 +7,4 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String LIBRARY_PACKAGE_NAME = "org.opencv";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 4010;
-  public static final String VERSION_NAME = "4.0.1";
 }
